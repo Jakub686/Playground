ac8d144d6890529c0df4bbc05e4e71754fa25d6b
Jakub <jakub.bchyla@gmail.com>
2021-12-01 15:59:35 +0100

    playing with code

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -1,9 +1,16 @@
 package com.company;
 
 public class PlayingWithCode {
+    public static void Sum(int a, int b) {
+        int c = a + b;
+        System.out.println(c);
+    }
 
+    public static void main(String[] args) {
+         Sum(2,2);
+    }
 }
-//
+
 
 
 
