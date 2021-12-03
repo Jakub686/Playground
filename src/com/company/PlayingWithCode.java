6d0349bea6ff46f05e932f41ae74da7a80484568
Jakub <jakub.bchyla@gmail.com>
2021-12-03 16:38:13 +0100

    playing with code

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -1,13 +1,14 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class PlayingWithCode {
 
     public static void main(String[] args) {
-        ArrayList  list1 = new ArrayList();
-        ArrayList  list2 = new ArrayList();
-        list1.add("100");
+        Stack list1 = new Stack();
+        Stack list2 = new Stack();
+        list1.push("100");
         list2 = list1;
         System.out.println(list1 + " " + list2);
 
