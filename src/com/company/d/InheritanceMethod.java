583be3cc8f720160ac98ad20c17af88748a26380
Jakub <jakub.bchyla@gmail.com>
2021-12-03 18:56:11 +0100

    playing with code

---
+++ src/com/company/d/InheritanceMethod.java
@@ -0,0 +1,18 @@
+package com.company.d;
+
+public class InheritanceMethod {
+}
+class Canine {
+    public double getAverageWeight() {
+        return 50;
+    }
+}
+class Wolf extends Canine {
+    public double getAverageWeight() {
+        return super.getAverageWeight()+20;
+    }
+    public static void main(String[] args) {
+        System.out.println(new Canine().getAverageWeight());
+        System.out.println(new Wolf().getAverageWeight());
+    }
+}
