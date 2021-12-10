41e5c5b1b098787805c89589196b8ee05ca97e20
Jakub <jakub.bchyla@gmail.com>
2021-12-10 14:30:29 +0100

    playing with code

---
+++ src/com/company/chapter5/polimorfizm/Polimorfizm.java
@@ -0,0 +1,22 @@
+package com.company.chapter5.polimorfizm;
+
+class Bird{
+    public void sing(){
+        System.out.println("tweet tweet tweet");
+    }
+
+}
+
+class Robin extends Bird{
+    public void sing(){
+        System.out.println("twiddledeedee");
+    }
+}
+
+public class Polimorfizm {
+    public static void main(String[] args) {
+        Robin r = new Robin();
+        r.sing();
+
+    }
+}
