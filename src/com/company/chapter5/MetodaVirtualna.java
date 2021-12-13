3d0c9727d606c03b1fec70582c7ed521eb37bc80
Jakub <jakub.bchyla@gmail.com>
2021-12-13 16:48:35 +0100

    Virtual methods

--- src/com/company/chapter5/MetodaVirtualna.java
+++ src/com/company/chapter5/MetodaVirtualna.java
@@ -1,24 +1,28 @@
 package com.company.chapter5;
 
-//Virtual Methods
-
+//Virtual Methods to metoda która może być nadpisana przez podklase
 class Animal {
     public void eat(){
         System.out.println("Animal is eating");
     }
 }
-
 class Lion extends Animal {
     public void eat(){
         System.out.println("Lion is eating");
     }
 }
-
 public class MetodaVirtualna {
     public static void main(String[] args) {
-        Animal animalAnimal = new Animal();
-        animalAnimal.eat();
-        Animal animalLion = new Lion();
-        animalLion.eat();
+        Animal animal = new Animal();
+        animal.eat();
+        Animal animal1 = new Lion(); // Referencja jest typu Animal, ale rzeczywisty obiekt jest typu, Lion.
+        animal1.eat(); // Metoda nadpisana przez klase
+        Lion lion = new Lion();
+        lion.eat();
+
+//        result:
+//        Animal is eating
+//        Lion is eating
+//        Lion is eating
     }
 }
