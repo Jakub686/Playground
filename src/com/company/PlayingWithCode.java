db90b215ce69199f3d76f04f78d9766717fbc07f
Jakub <jakub.bchyla@gmail.com>
2021-12-22 14:44:46 +0100

    Playing with code

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -1,6 +1,17 @@
 package com.company;
 
 
-public class PlayingWithCode {}
+public class PlayingWithCode {
+    public static void main(String[] args) {
+
+
+        int i = 0;
+        int j = 1;
+        if ((i++ == 0) & (j++ == 2)) {
+            i = 12;
+        }
+
+    }
+}
 
 
