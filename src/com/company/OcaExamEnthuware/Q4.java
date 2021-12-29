1c46b152a3236cf9754b37e12cd859b298119eba
Jakub <jakub.bchyla@gmail.com>
2021-12-29 15:10:10 +0100

    Playing with code

---
+++ src/com/company/OcaExamEnthuware/Q4.java
@@ -0,0 +1,15 @@
+package com.company.OcaExamEnthuware;
+public class Q4 {
+}
+class TestQ4{
+    public static void testInts(Integer a, int b){
+        a = b++;
+        a++;
+    }
+    public static void main(String[] args) {
+        Integer a = new Integer(5);
+        int b = 9;
+        testInts(a++, ++b);
+        System.out.println(a+" "+b);
+    }
+}
