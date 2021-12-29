6baa001458c7c34fe683f8b20f1a95735cdd3d24
Jakub <jakub.bchyla@gmail.com>
2021-12-29 14:09:31 +0100

    Playing with code

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -2,17 +2,9 @@ package com.company;
 
 
 public class PlayingWithCode {
-    static int[] data = {1, 2, 3, 4, 5};
-
     public static void main(String[] args) {
-        for (int i : data) {
-            if (i < 2) {
-                continue;
-            }
-            System.out.print(i);
-            if (i == 3) {
-                //insert code2 here
-            }
-        }
+        char ch = 10;
+        System.out.println(ch);
     }
 }
+
