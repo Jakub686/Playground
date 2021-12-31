87f4af5383dc7c33b290bb6864b8eb256920a00d
Jakub <jakub.bchyla@gmail.com>
2021-12-31 10:04:29 +0100

    Playing with code

---
+++ src/com/company/OcaExamEnthuware/Q8.java
@@ -0,0 +1,24 @@
+package com.company.OcaExamEnthuware;
+
+public class Q8 {
+}
+class Square {
+    private double side = 0;
+    String color;
+    public Square(double length){
+        this.side = length;
+    }
+    public double getSide() {  return side;    }
+
+    public void setSide(double side) {  this.side = side;   }
+
+}
+
+ class TestClassss {
+    public static void main(String[] args) throws Exception {
+        Square mysq = new Square(10);
+        mysq.color = "red";
+
+        //set mysq's side to 20
+    }
+}
