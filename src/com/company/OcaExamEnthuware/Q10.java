13a67af76dde04686e25c98ff0a60c3919acf798
Jakub <jakub.bchyla@gmail.com>
2022-01-01 12:15:19 +0100

    Playing with code

---
+++ src/com/company/OcaExamEnthuware/Q10.java
@@ -0,0 +1,20 @@
+package com.company.OcaExamEnthuware;
+
+public class Q10 {
+}
+
+class TestClasssssssssssss{
+    public void method(Object o){
+        System.out.println("Object Version");
+    }
+    public void method(java.io.FileNotFoundException s){
+        System.out.println("java.io.FileNotFoundException Version");
+    }
+    public void method(java.io.IOException s){
+        System.out.println("IOException Version");
+    }
+    public static void main(String args[]){
+        TestClass tc = new TestClass();
+        //tc.method(null);
+    }
+}
