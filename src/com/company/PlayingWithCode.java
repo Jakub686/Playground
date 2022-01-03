9d42cf9d725e8051885267c5ec6de0cca3437215
Jakub <jakub.bchyla@gmail.com>
2022-01-03 07:24:09 +0100

    Playing with code

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -1,7 +1,9 @@
 package com.company;
 
 public class PlayingWithCode {
-
+    public static void main(String[] args) {
+        new StringBuilder("world").insert(0, "hello ").toString();
+    }
 }
 
 
