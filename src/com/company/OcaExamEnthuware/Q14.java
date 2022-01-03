6e227129f44e529632713c9d4521e89c719b4684
Jakub <jakub.bchyla@gmail.com>
2022-01-03 07:56:20 +0100

    Playing with code

---
+++ src/com/company/OcaExamEnthuware/Q14.java
@@ -0,0 +1,8 @@
+package com.company.OcaExamEnthuware;
+
+public class Q14 {
+    public static void main(String[] args) {
+        String a = "abcdef";
+        System.out.println(a.charAt(3));
+    }
+}
