d0c55ada4ea716382c629330d0f940199e8aaf23
Jakub <jakub.bchyla@gmail.com>
2022-01-05 17:49:33 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q29.java
@@ -0,0 +1,21 @@
+package com.company.OcaExamEnthuware;
+
+public class Q29 {
+
+    public static void loopTest(int x) {
+        loop:
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                System.out.println(i);
+                if (x == 0) {
+                    continue loop;
+                }
+                System.out.println(j);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        loopTest(0);
+    }
+}
