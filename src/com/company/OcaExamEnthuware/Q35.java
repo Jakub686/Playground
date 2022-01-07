09e83b1e12bb7136753ceea39e717ea875230478
Jakub <jakub.bchyla@gmail.com>
2022-01-07 08:28:30 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q35.java
@@ -0,0 +1,12 @@
+package com.company.OcaExamEnthuware;
+
+public class Q35 {
+    public static void main(String args[]){
+        int i;
+        int j;
+        for (i = 0, j = 0; j < i; ++j, i++){
+            System.out.println(i + " " + j);
+        }
+        System.out.println(i + " " + j);
+    }
+}
