cbee33fbf2265df87524be8cac2df4a05c0b2d89
Jakub <jakub.bchyla@gmail.com>
2022-01-09 11:52:32 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q41.java
@@ -0,0 +1,10 @@
+package com.company.OcaExamEnthuware;
+
+public class Q41 {
+    public static void main(String args[ ] ){
+        StringBuilder sb = new StringBuilder("12345678");
+        sb.setLength(5);
+        sb.setLength(10);
+        System.out.println(sb.length());
+    }
+}
