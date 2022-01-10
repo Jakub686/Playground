cf857092dbd6c97c62ed3af92839a587fb50a13e
Jakub <jakub.bchyla@gmail.com>
2022-01-10 14:45:16 +0100

    OCA exam preparations

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -1,22 +1,15 @@
 package com.company;
 
 public class PlayingWithCode {
-}
-class A1 {
-}
+    public static void main(String[] args) {
 
-class AA1 extends A1 {
-}
-
-
-class TestC {
-    public static void main(String[] args) throws Exception {
-        A1 a = new A1();
-        AA1 aa = new AA1();
-        a = aa;
-        System.out.println("a = "+a.getClass());
-        System.out.println("aa = "+aa.getClass());
+        int i = 1;
+        do {
+            i++;
+        } while (i==i);
+        System.out.println(i);
     }
+
 }
 
 
