0e66dc08dfba79fd55c328d527e1b1f32de78b98
Jakub <jakub.bchyla@gmail.com>
2022-01-11 18:28:09 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q45.java
@@ -0,0 +1,22 @@
+package com.company.OcaExamEnthuware;
+
+public class Q45 {
+    public static void main(String args[]){
+        int i = 0;
+        loop :         // 1
+        {
+            System.out.println("Loop Lable line");
+            try{
+                for (  ;  true ;  i++ ){
+                    if( i >5) break loop;       // 2
+                }
+            }
+            catch(Exception e){
+                System.out.println("Exception in loop.");
+            }
+            finally{
+                System.out.println("In Finally");      // 3
+            }
+        }
+    }
+}
--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -3,11 +3,7 @@ package com.company;
 public class PlayingWithCode {
     public static void main(String[] args) {
 
-        int i = 1;
-        do {
-            i++;
-        } while (i==i);
-        System.out.println(i);
+        System.out.println('s'+'a');
     }
 
 }
