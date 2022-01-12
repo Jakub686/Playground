36f655fbe07eceadbc697e1f943e027641a8b763
Jakub <jakub.bchyla@gmail.com>
2022-01-12 17:01:06 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q47.java
@@ -0,0 +1,17 @@
+package com.company.OcaExamEnthuware;
+
+public class Q47 {
+    private int j;
+    void showJ(){
+        while(j<=5){
+            for(int j=1; j <= 5;){
+                System.out.print(j+" ");
+                j++;
+            }
+            j++;
+        }
+    }
+    public static void main(String[] args) {
+        new Q47().showJ();
+    }
+}
--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -3,7 +3,10 @@ package com.company;
 public class PlayingWithCode {
     public static void main(String[] args) {
 
-        System.out.println('s'+'a');
+        for(int i=0; i<10;){
+            i=i++;
+            System.out.println(i);
+        }
     }
 
 }
