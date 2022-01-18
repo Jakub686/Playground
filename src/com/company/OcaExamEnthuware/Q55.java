6a20c2958a099e197341143ef1f376373a58c82a
Jakub <jakub.bchyla@gmail.com>
2022-01-18 15:45:00 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q55.java
@@ -0,0 +1,10 @@
+package com.company.OcaExamEnthuware;
+
+public class Q55 {
+    public static void main(String[] args){
+        for (int i = 0; i < 10; i++) System.out.print(i + " ");  //1
+        for (int i = 10; i > 0; i--) System.out.print(i + " ");  //2
+        int i = 20;                  //3
+        System.out.print(i + " ");   //4
+    }
+}
