c469d35cb59487ea26cbf944215e4ca4df46f38c
Jakub <jakub.bchyla@gmail.com>
2022-01-18 16:45:18 +0100

    OCA exam preparations

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -2,10 +2,8 @@ package com.company;
 
 public class PlayingWithCode {
     public static void main(String[] args) {
-
-        boolean[] b1 = new boolean[2];
-        boolean[] b2 = {true , false};
-        System.out.println( "rterter" + (b1[0] == b2[0]) + ", "+ (b1[1] == b2[1])  );
+        boolean b = false|true;
+        System.out.println(b);
     }
 
 }
