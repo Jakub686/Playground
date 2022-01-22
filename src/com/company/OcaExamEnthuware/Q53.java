df1d5fa3e6c516f5b2a1879b9ec8cfcbdd02df33
Jakub <jakub.bchyla@gmail.com>
2022-01-22 11:20:30 +0100

    OCA exam preparations

--- src/com/company/OcaExamEnthuware/Q53.java
+++ src/com/company/OcaExamEnthuware/Q53.java
@@ -1,16 +1,19 @@
 package com.company.OcaExamEnthuware;
 
 public class Q53 {
-    public static void main(String args[]){
-        int c = 0;
-        A: for(int i = 0; i < 2; i++){
-            B: for(int j = 0; j < 2; j++){
-                C: for(int k = 0; k < 3; k++){
-                    c++;
-                    if(k>j) break;
+    public static void main(String args[]) {
+        int k = 9, s = 5;
+        switch (k) {
+            default:
+                if (k == 10) {
+                    s = s * 2;
+                } else {
+                    s = s + 4;
+                    break;
                 }
-            }
+            case 7:
+                s = s + 3;
         }
-        System.out.println(c);
+        System.out.println(s);
     }
 }
