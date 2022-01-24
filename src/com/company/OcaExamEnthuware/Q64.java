2e5463b93cfb6703404ddefc551f4090f8e078ad
Jakub <jakub.bchyla@gmail.com>
2022-01-24 15:28:24 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q64.java
@@ -0,0 +1,14 @@
+package com.company.OcaExamEnthuware;
+
+public class Q64 {
+    public static void main(String[] args) {
+        String[] sa = {"a", "b", "c"};
+        for (String s : sa) {
+            if ("b".equals(s)) continue;
+            System.out.println(s);
+            if ("b".equals(s)) break;
+            System.out.println(s + " again");
+
+        }
+    }
+}
---
+++ src/com/company/OcaExamEnthuware/Q65.java
@@ -0,0 +1,10 @@
+package com.company.OcaExamEnthuware;
+
+public class Q65 {
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 0};
+        for (int i : arr) {
+            System.out.println(arr[i]);
+        }
+    }
+}
