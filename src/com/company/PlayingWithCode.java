b84be33e8f9665712d84fcbcd81900b2c6132843
Jakub <jakub.bchyla@gmail.com>
2022-01-26 15:10:51 +0100

    OCA exam preparations

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -4,9 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlayingWithCode {
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder(100);
-        System.out.println(sb.length() + ":" + sb.toString().length());
+    public static void main(String[] args){
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int counter = 0;
+        for (int value : arr) {
+            if (counter >= 5) {
+                break;
+            } else {
+                continue;
+            }
+//            if (value > 4) {
+//                arr[counter] = value + 1;
+//            }
+//            counter++;
+        }
+        System.out.println(arr[counter]);
     }
 }
 
