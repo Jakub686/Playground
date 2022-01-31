da43a7f23c4976a89dbab01c5703e1ee38b267c0
Jakub <jakub.bchyla@gmail.com>
2022-01-31 04:59:30 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q79.java
@@ -0,0 +1,19 @@
+package com.company.OcaExamEnthuware;
+
+public class Q79 {
+
+        public static void main(String[] args) {
+            byte var = 100;
+            switch(var) {
+                case 100:
+                    System.out.println("var is 100");
+                    break;
+//                case 200:
+//                    System.out.println("var is 200");
+//                    break;
+                default:
+                    System.out.println("In default");
+            }
+        }
+            //byte jest do 128
+}
