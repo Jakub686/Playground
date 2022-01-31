c59f863fdab19e456bdd3476ddc11f786fb0db54
Jakub <jakub.bchyla@gmail.com>
2022-01-31 17:06:52 +0100

    OCA exam preparations

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -4,8 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlayingWithCode {
+
+
     public static void main(String[] args) {
-        String s = 63+"a";
-        System.out.println(s);
+        Integer Integera;
+        int integer=1;
+        String one, two;
+        one = new String("a");
+        two = new String("b");
+        one = two;
+        String three = one;
+        one = null;
+            System.out.println(one);
+            System.out.println(two);
+            System.out.println(three);
+            System.out.println(integer);
     }
 }
