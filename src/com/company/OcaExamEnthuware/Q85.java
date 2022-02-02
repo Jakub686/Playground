b9c72da0655a87270befef99179d8770f16c7514
Jakub <jakub.bchyla@gmail.com>
2022-02-02 17:39:47 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q85.java
@@ -0,0 +1,12 @@
+package com.company.OcaExamEnthuware;
+
+public class Q85 {
+    public static void main(String[] args) {
+        do {
+            System.out.println(100);
+        } while (true);
+        //w while nie moze byc samo true, kompliator wywali blad jesli odrazu bedzie samo true
+
+        //System.out.println("Bye");
+    }
+}
