cb87b87cadfb62dd754911b0f9e726edb29d2836
Jakub <jakub.bchyla@gmail.com>
2022-02-03 16:48:01 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q86.java
@@ -0,0 +1,18 @@
+package com.company.OcaExamEnthuware;
+
+public class Q86 {
+    public static void main(String[] args) {
+        char [][] arr = {
+                {'A', 'B', 'C'},
+                {'D', 'E', 'F'},
+                {'G', 'H', 'I'}
+        };
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][1]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/com/company/OcaExamEnthuware/Q88.java
@@ -0,0 +1,10 @@
+package com.company.OcaExamEnthuware;
+
+public class Q88 {
+    static Double d1;
+    static int x = d1.intValue();
+
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
+}
