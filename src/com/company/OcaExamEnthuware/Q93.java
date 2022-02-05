6da677a58743c38c7ef87b42934aa8ed680a8083
Jakub <jakub.bchyla@gmail.com>
2022-02-05 07:47:47 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q93.java
@@ -0,0 +1,10 @@
+package com.company.OcaExamEnthuware;
+
+public class Q93 {
+    public static void main(String[] args){
+        if (args[0].equals("open"))
+            if (args[1].equals("someone"))
+                System.out.println("Hello!");
+            else System.out.println("Go away "+ args[1]);
+    }
+}
