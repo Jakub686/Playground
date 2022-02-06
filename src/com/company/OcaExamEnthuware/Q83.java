8c723af32e64a5c556d4033c28317693418576b1
Jakub <jakub.bchyla@gmail.com>
2022-02-06 08:30:30 +0100

    OCA exam preparations

--- src/com/company/OcaExamEnthuware/Q83.java
+++ src/com/company/OcaExamEnthuware/Q83.java
@@ -2,12 +2,16 @@ package com.company.OcaExamEnthuware;
 
 public class Q83 {
     public static void main(String[] args) {
-        short b = 1;
-        byte s = 1;
+        short s = 1;
+        byte b = 2;
+        double d = 3;
 
         //s = s * b ; //operands of mathematical operators are ALWAYS promoted to AT LEAST int. dlatego wynik s * b bedzie int wiec nie mozna go przypisac do s bo s to byte
         System.out.println(s);
         s *= b ; // dlaczego to sie kompiluje?
         System.out.println(s);
+        b*= d; // ?????
+        System.out.println(b);
+        // jak sprawdzidz jakim typem jest zmienna
     }
 }
