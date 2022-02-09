d6319fbe8ba097821d107c287491d0180846a7ec
Jakub <jakub.bchyla@gmail.com>
2022-02-09 05:04:37 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q98.java
@@ -0,0 +1,9 @@
+package com.company.OcaExamEnthuware;
+
+public class Q98 {
+    public static void main(String[] args) throws Exception{
+        int a = Integer.MIN_VALUE;
+        int b = -a;
+        System.out.println( a+ "   "+b);
+    }
+}
