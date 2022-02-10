712ad04e3f580b8cad8bb70c7b8c7ef2b3695d35
Jakub <jakub.bchyla@gmail.com>
2022-02-10 15:31:35 +0100

    OCA exam preparations

--- src/com/company/OcaExamEnthuware/Q99.java
+++ src/com/company/OcaExamEnthuware/Q99.java
@@ -2,16 +2,21 @@ package com.company.OcaExamEnthuware;
 
 public class Q99 {
 }
-public class TestClss{
-    Q99 getMyClassObject(){
-        Q99 mc = new Q99(); //1
+
+class MyClass {
+}
+
+class TestCl {
+    MyClass getMyClassObject() {
+        MyClass mc = new MyClass(); //1
         return mc; //2
     }
-    public static void main(String[] args){
-        TestClss tc = new TestClss(); //3
-        Q99 x = tc.getMyClassObject(); //4
+
+    public static void main(String[] args) {
+        TestCl tc = new TestCl(); //3
+        MyClass x = tc.getMyClassObject(); //4
         System.out.println("got myclass object"); //5
-        x = new Q99(); //6
+        x = new MyClass(); //6
         System.out.println("done"); //7
     }
 }
