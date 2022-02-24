f1064ea6535dade78d38b41748fb9b1c2256d9a9
Jakub <jakub.bchyla@gmail.com>
2022-02-24 16:06:07 +0100

    OCA exam preparations

--- src/com/company/OcaExamEnthuware/PlayingWithCodeQ2.java
+++ src/com/company/OcaExamEnthuware/PlayingWithCodeQ2.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 public class PlayingWithCodeQ2 {
     public static void main(String[] args) {
-        System.out.println(new Boolean("ture"));
+        int [] arr1 = {5, 10, 15};
+        int [] arr2 = {'A', 'B'};
+        arr1 = arr2;
+        System.out.println(arr1.length + arr2.length);
     }
- }
 
 
-
-
+}
