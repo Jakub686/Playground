5ca41c32c1708a5f8eba7ed20b819fcafbaa3e35
Jakub <jakub.bchyla@gmail.com>
2022-02-25 13:31:13 +0100

    OCA exam preparations

--- src/com/company/OcaExamEnthuware/PlayingWithCodeQ2.java
+++ src/com/company/OcaExamEnthuware/PlayingWithCodeQ2.java
@@ -5,10 +5,8 @@ import java.util.List;
 
 public class PlayingWithCodeQ2 {
     public static void main(String[] args) {
-        int [] arr1 = {5, 10, 15};
-        int [] arr2 = {'A', 'B'};
-        arr1 = arr2;
-        System.out.println(arr1.length + arr2.length);
+    double d = 5.5;
+        System.out.println(d%2);
     }
 
 
