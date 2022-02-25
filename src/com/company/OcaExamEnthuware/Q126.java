6cb754287921a4457418150066fec7cbafc49f14
Jakub <jakub.bchyla@gmail.com>
2022-02-25 15:03:54 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q126.java
@@ -0,0 +1,15 @@
+package com.company.OcaExamEnthuware;
+
+public class Q126 {
+
+    public static void change(int num) {
+        num++;
+        System.out.println(num);
+    }
+
+    public static void main(String[] args) {
+        int i1 = 1;
+        Q126.change(i1);
+        System.out.println(i1);
+    }
+}
