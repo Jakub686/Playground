d7a4ac9e479650c41bcc70656e96fb9c690bc7ee
Jakub <jakub.bchyla@gmail.com>
2022-02-26 08:47:38 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q127.java
@@ -0,0 +1,10 @@
+package com.company.OcaExamEnthuware;
+
+import java.time.LocalDate;
+
+public class Q127 {
+    public static void main(String [] args) {
+        LocalDate joiningDate = LocalDate.parse("2006-03-16");
+        System.out.println(joiningDate.withDayOfYear(29));
+    }
+}
