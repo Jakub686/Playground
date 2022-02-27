1273f7003ca3de40fe1e31e3a7a1c1b117d64d84
Jakub <jakub.bchyla@gmail.com>
2022-02-27 08:51:13 +0100

    OCA exam preparations

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -3,22 +3,15 @@ package com.company;
 
 import com.company.zadaniarekrutacyjne.TestClass;
 
+import java.time.LocalDate;
+
 public class PlayingWithCode {
 
-    public static void main(String[] args) {
-        int x = 1;
-        while(checkAndIncrement(x)) {
-            System.out.println(x);
-        }
-    }
 
-    private static boolean checkAndIncrement(int x) {
-        if(x < 5) {
-            x++;
-            return true;
-        } else {
-            return false;
+        public static void main (String[]args){
+            LocalDate date = LocalDate.of(2020, 9, 6);
+            System.out.println(date);
         }
-    }
+
+
 }
-
