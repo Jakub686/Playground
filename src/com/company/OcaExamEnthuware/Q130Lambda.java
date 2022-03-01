c3ce2cd4206a15d78e342a38ca6ee2bb20f22764
Jakub <jakub.bchyla@gmail.com>
2022-03-01 18:38:53 +0100

    OCA exam preparations

---
+++ src/com/company/OcaExamEnthuware/Q130Lambda.java
@@ -0,0 +1,25 @@
+package com.company.OcaExamEnthuware;
+
+import java.util.ArrayList;
+
+public class Q130Lambda {
+    public static void main(String[] args) {
+        ArrayList<String>  shoppinglist = new ArrayList<>();
+        shoppinglist.add("meat");
+        shoppinglist.add("cheese");
+        shoppinglist.add("veges");
+        shoppinglist.add("yogurt");
+
+        shoppinglist.forEach((n)-> System.out.println(n));
+        /*
+        Interface funkcyjny (funkcja ktore nie nalezy do klasy, funkcja ktora moze byc stosowane jako wartosc zmiennej)
+        parameter list - > body funcion
+        usuwamy z metody:
+        - modifiers
+        - name
+        - return type
+        + ->
+         */
+
+    }
+}
