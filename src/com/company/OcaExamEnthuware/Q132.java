fea06814c80e45feeb58491f0f1b36bde0044a07
Jakub <jakub.bchyla@gmail.com>
2022-03-05 06:59:11 +0100

    OCA exam preparations

--- src/com/company/OcaExamEnthuware/Q132.java
+++ src/com/company/OcaExamEnthuware/Q132.java
@@ -1,6 +1,16 @@
 package com.company.OcaExamEnthuware;
 
 public class Q132 {
+    public static void main(String[] args) {
+        boolean flag = false;
+        do {
+            if(flag = !flag) { //Line n1
+                System.out.print(1); //Line n2
+                continue; //Line n3
+            }
+            System.out.print(2); //Line n4
+        } while(flag); //Line n5
+    }
 }
 
 
