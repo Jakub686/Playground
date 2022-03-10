ced31951f4b9e6a91cb836c560bcdbefa1cc98e6
Jakub <jakub.bchyla@gmail.com>
2022-03-10 07:50:19 +0100

    OCA exam preparations

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -1,18 +1,29 @@
 package com.company;
 
-
-import com.company.zadaniarekrutacyjne.TestClass;
-
-import java.time.LocalDate;
+import java.util.Arrays;
 
 public class PlayingWithCode {
-
-
     public static void main(String[] args) {
-        String string ="-1312";
-        int count = string.length() - string.replaceAll("2"," ").length();
-        System.out.println(count);
+        int[] a = {2, 1, 2, 3, 3, 5, 5, 1, 2, 4, 4, 20, 20, -10};
+        int[][] c = new int[a.length][2];
+        int[] b = new int[200];
+        for(int i = 0; i<a.length;i++){
+            c[i][0]=a[i];
+        }
 
+        for (int i = -100; i <= 99; i++) { //zakres
+            b[i + 100] = i;
+        }
+        for (int i = 0; i <= 200; i++){
+            for (int j = 0; j == a.length-1; j++) {
+                if (b[i] == a [j]){
+                    c[j][1]++;
+                }
+            }
+        }
+        for (int[] arr : c) {
+            System.out.println(Arrays.toString(arr));
+        }
 
     }
 }
