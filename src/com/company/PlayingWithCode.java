453b8ab50541e9e729cba28a64ce4ab5b8489ae9
Jakub <jakub.bchyla@gmail.com>
2022-03-17 18:12:34 +0100

    OCA exam preparations

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -1,11 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class PlayingWithCode {
-    static String str;
+
     public static void main(String[] args) {
-        System.out.println(str);
 
     }
+
 }
--- src/com/company/chapter4/OverRIde.java
+++ src/com/company/chapter4/OverRIde.java
@@ -12,11 +12,11 @@ class Dog extends Animalss {
         System.out.println("the dog makes noise");
     }
 
-    void makeNoise(int asdf) {
-        System.out.println("the dog makes noise");
-    }
+
 }
 
+
+// Return type must be the same except when  super method returns Object and sub method returns String
 public class OverRIde {
     public static void main(String[] args) {
         Animalss animal = new Animalss();
