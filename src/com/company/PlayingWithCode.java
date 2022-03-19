0ff75f61146093983cd344887d27644e25b05eab
Jakub <jakub.bchyla@gmail.com>
2022-03-19 07:33:12 +0100

    OCA exam preparations

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -7,14 +7,8 @@ import java.util.HashMap;
 public class PlayingWithCode {
 
     public static void main(String[] args) {
-            int i = 5;
-            float f = 5.5f;
-            double d = 3.8;
-            char c = 'a';
-            if (i == f) c++;
-            if (((int) (f + d)) == ((int) f + (int) d)) c += 2;
-            System.out.println(c);
-            //(int)(f + d) => (int) (5.5 + 3.8) => (int)(9.3) => 9, so this also return false.
+        double f = 43e-1;
+        System.out.println(f);
     }
 
 }
