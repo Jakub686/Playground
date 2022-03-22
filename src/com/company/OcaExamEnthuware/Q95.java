aa2cccd95f426a37b8fe1ceecbccd63f5932d67b
Jakub <jakub.bchyla@gmail.com>
2022-03-22 15:14:59 +0100

    OCA exam preparations

--- src/com/company/OcaExamEnthuware/Q95.java
+++ src/com/company/OcaExamEnthuware/Q95.java
@@ -1,29 +1,24 @@
 package com.company.OcaExamEnthuware;
 
 public class Q95 {
-    public static void testInts(Integer obj, int variable) {
+    public static void testInts(Integer obj, int variable, int[] array) {
 
         variable++;
         obj++;
-        System.out.println(obj + " " + variable);
+        array[0]++;
+        System.out.println("In method     " + obj + " " + variable + " " + array[0]);
         //For Primitive types, parameters are pass-by-value
         //For Object types, the object reference is pass-by-value
 
-    }
-    public static void testInts2(Integer obj, int variable) {
-
-        variable++;
-        obj++;
-        System.out.println(obj + " " + variable);
-        //For Primitive types, parameters are pass-by-value
-        //For Object types, the object reference is pass-by-value
 
     }
+
     public static void main(String[] args) {
         Integer obj = new Integer(5);
-        int variable = 9;
-        System.out.println(obj + " " + variable);
-        testInts(obj, variable);
-        System.out.println(obj + " " + variable);
+        int variable = 10;
+        int[] array = {20};
+        System.out.println("Before method " + obj + " " + variable + " " + array[0]);
+        testInts(obj, variable, array);
+        System.out.println("After method  " + obj + " " + variable + " " +array[0]);
     }
 }
