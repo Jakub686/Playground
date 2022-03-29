c14bee0c7127383a471a46beedffb180b72d7f9f
Jakub <jakub.bchyla@gmail.com>
2022-03-29 18:04:44 +0200

    OCA exam preparations

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -1,21 +1,10 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 
 public class PlayingWithCode {
-    static String a;
+    public static void main(String args[]){
 
-    public static void main(String[] args) {
-        System.out.println(PlayingWithCode.a);
-        try{
-            RuntimeException re = null;
-            throw re;
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
+        System.out.println(3);
 
 
     }
