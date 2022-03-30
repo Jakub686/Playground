3b901f0dce83c7a8b72b57db3f1b569d459c0b95
Jakub <jakub.bchyla@gmail.com>
2022-03-30 15:09:37 +0200

    OCA exam preparations

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -3,8 +3,17 @@ package com.company;
 
 public class PlayingWithCode {
     public static void main(String args[]){
+        byte BYTE = 'A' ;
+        short SHORT = 'A';
+        int INT = 'A';
+        long LONG = 'A';
+        float FLOAT = 'A';
+        double DOUBLE = 'A';
+        char CHAR = "a";
 
-        System.out.println(3);
+
+
+        System.out.println(DOUBLE);
 
 
     }
