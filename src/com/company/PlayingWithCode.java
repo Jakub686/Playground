433d98c2a9c9bbe7b1e666e33688d146a67b8315
Jakub <jakub.bchyla@gmail.com>
2022-04-03 09:18:20 +0200

    OCA exam preparations

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -13,6 +13,10 @@ public class PlayingWithCode {
         //short s = c;
         char c = 'a';
         INT = CHAR;
+        String[] arr = new String[2];
+        System.out.println(arr[1]);
+        Integer con = new Integer(1);
+        Short s = new Short((short)1);
 
 
 
