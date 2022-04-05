c4f3f73647ea0e87f4f05456d3f895896016dd4b
Jakub <jakub.bchyla@gmail.com>
2022-04-05 15:21:48 +0200

    OCA exam preparations

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -2,26 +2,8 @@ package com.company;
 
 
 public class PlayingWithCode {
-    public static void main(String args[]){
-        byte BYTE = 'A' ;
-        short SHORT = 'A';
-        int INT = 'A';
-        long LONG = 'A';
-        float FLOAT = 'A';
-        double DOUBLE = 'A';
-        char CHAR = 'a';
-        //short s = c;
-        char c = 'a';
-        INT = CHAR;
-        String[] arr = new String[2];
-        System.out.println(arr[1]);
-        Integer con = new Integer(1);
-        Short s = new Short((short)1);
-
-
-
-        System.out.println(DOUBLE);
-
-
+    public static void main(String args[]) {
+        String s = "10"+10; System.out.print(s);
     }
 }
+
