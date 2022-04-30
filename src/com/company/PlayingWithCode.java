063a14dce8c3b9bb52ab8d0e3b9b9ad87714ec29
Jakub <jakub.bchyla@gmail.com>
2022-04-30 06:55:50 +0200

    2

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -1,22 +1,23 @@
 package com.company;
 
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 public class PlayingWithCode {
 
-    public static final String DEFAULT_APPLICATION_NAME = "MyDemoApp";
-    public static final int MAX_IMAGE_SIZE_KB = 4096;
 
 
-    public static void main(String args[]) throws IOException {
-        File file = new File("D:\\file.txt");
-        FileWriter writer = new FileWriter(file,true); // overwrites the file
+    public static void main(String args[]) {
 
-        writer.write("Hello, World\n");
-        writer.close();
+        String a = "012345748";
+
+        String b = a.substring(3);
+        System.out.println(b);
+
 
     }
 }
