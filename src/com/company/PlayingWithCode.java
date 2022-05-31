8199386414049ed57f2ab5a21627c7c1996bb3a1
Jakub <jakub.bchyla@gmail.com>
2022-05-31 16:34:26 +0200

    buffer writer

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -1,21 +1,15 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
+import java.util.Map;
 
 public class PlayingWithCode {
-    public static void main(String args[]) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\java examples\\java\\play\\src\\com\\company\\bufferWriter\\sb.json"));
-            writer.write("bla bla");
-            writer.close();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-
+    public static void main(String[] args) {
+        String key = "[person]";
+        key = key.substring(1,7);
+        System.out.println(key);
     }
+
 
 }
