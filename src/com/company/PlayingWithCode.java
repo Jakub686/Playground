3185f8ee2be5932f77a86015b70100e7621e1290
Jakub <jakub.bchyla@gmail.com>
2022-07-03 10:03:14 +0200

    a

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
@@ -10,13 +11,7 @@ import java.util.regex.Matcher;
 public class PlayingWithCode {
     public static void main(String[] args) {
 
-        String s = "A";
-        char c = s.charAt(0);
-
-        System.out.println(c);
-
-
-
+        ArrayList arrayList = new ArrayList();
 
 
 
