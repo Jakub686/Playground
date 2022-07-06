52aa023229bd9db7ad13d63368972ceaca446ab9
Jakub <jakub.bchyla@gmail.com>
2022-07-06 17:19:09 +0200

    a

--- src/com/company/PlayingWithCode.java
+++ src/com/company/PlayingWithCode.java
@@ -11,11 +11,21 @@ import java.util.regex.Matcher;
 public class PlayingWithCode {
     public static void main(String[] args) {
 
-        ArrayList arrayList = new ArrayList();
-        arrayList.add("2");
-        arrayList.add("aaa");
+        ArrayList arrayList1 = new ArrayList();
+        ArrayList arrayList2 = new ArrayList();
+        arrayList1.add("111");
+        arrayList1.add("aaa");
+
+        arrayList2.add("222");
+        arrayList2.add("bbb");
+
+        show(arrayList1);
+        show(arrayList2);
 
 
+    }
 
+    public static void show(ArrayList arrayList){
+        System.out.println(arrayList);
     }
 }
