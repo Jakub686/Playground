4689219470ccd61a28a0860d3bd3fd5f5398c13d
Jakub <jakub.bchyla@gmail.com>
2022-07-10 15:18:12 +0200

    a

--- src/com/company/StringMatchEvaluation.java
+++ src/com/company/StringMatchEvaluation.java
@@ -3,22 +3,12 @@ package com.company;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
-
-public class PlayingWithCode {
-
+public class StringMatchEvaluation {
     public static void main(String[] args) throws ScriptException {
         ScriptEngineManager mgr = new ScriptEngineManager();
         ScriptEngine engine = mgr.getEngineByName("JavaScript");
         String foo = "40+2";
-        //System.out.println(engine.eval(foo));
+        System.out.println(engine.eval(foo));
     }
-
 }
